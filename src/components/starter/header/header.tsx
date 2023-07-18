dcaa30fac5e5969b26a967e65e0a3b009e411898
ulic75 <ulic75@gmail.com>
2023-07-18 08:34:47 -0600

    add auth to header

--- src/components/starter/header/header.tsx
+++ src/components/starter/header/header.tsx
@@ -1,11 +1,14 @@
-import { component$ } from "@builder.io/qwik";
-import { QwikLogo } from "../icons/qwik";
-import styles from "./header.module.css";
+import { component$ } from '@builder.io/qwik';
+import { QwikLogo } from '../icons/qwik';
+import styles from './header.module.css';
+import { useAuthSession } from '~/routes/plugin@auth';
 
 export default component$(() => {
+  const session = useAuthSession();
+
   return (
     <header class={styles.header}>
-      <div class={["container", styles.wrapper]}>
+      <div class={['container', styles.wrapper]}>
         <div class={styles.logo}>
           <a href="/" title="qwik">
             <QwikLogo height={50} width={143} />
@@ -13,29 +16,29 @@ export default component$(() => {
         </div>
         <ul>
           <li>
-            <a
-              href="https://qwik.builder.io/docs/components/overview/"
-              target="_blank"
-            >
+            <a href="https://qwik.builder.io/docs/components/overview/" target="_blank">
               Docs
             </a>
           </li>
           <li>
-            <a
-              href="https://qwik.builder.io/examples/introduction/hello-world/"
-              target="_blank"
-            >
+            <a href="https://qwik.builder.io/examples/introduction/hello-world/" target="_blank">
               Examples
             </a>
           </li>
           <li>
-            <a
-              href="https://qwik.builder.io/tutorial/welcome/overview/"
-              target="_blank"
-            >
+            <a href="https://qwik.builder.io/tutorial/welcome/overview/" target="_blank">
               Tutorials
             </a>
           </li>
+          {session.value?.user ? (
+            <li>
+              <a href="/api/auth/signout">Sign Out</a>
+            </li>
+          ) : (
+            <li>
+              <a href="/api/auth/signin">Sign In</a>
+            </li>
+          )}
         </ul>
       </div>
     </header>
